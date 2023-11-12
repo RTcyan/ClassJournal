30c3cdbf17319e291632c2ffa4bb72e469c15006
Maxim <mgudkov@digital-spirit.ru>
2023-11-13 00:22:20 +0300

    change grade model

--- Domain/Model/Grade.cs
+++ Domain/Model/Grade.cs
@@ -4,6 +4,7 @@ public class Grade
 	public Guid Id { get; set; }
 	public string Name { get; set; } = string.Empty;
 	public GradeType GradeType { get; set; } = null!;
+	public Teacher GradeTeacher { get; set; } = null!;
 }
 
 
